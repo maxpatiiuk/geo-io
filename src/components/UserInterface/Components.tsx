7c7e6a571098120fb3d5d4bfc5c06611110d127a
Max Patiiuk <max@patii.uk>
2025-02-01 13:36:18 -0800

    chore: update to Tailwind v4

--- src/components/UserInterface/Components.tsx
+++ src/components/UserInterface/Components.tsx
@@ -1,2 +1,2 @@
 export const buttonClassName =
-  'bg-white hover:bg-gray-200 p-4 rounded text-black';
+  'bg-white hover:bg-gray-200 p-4 rounded-sm text-black';
--- src/components/UserInterface/GameOverlay.tsx
+++ src/components/UserInterface/GameOverlay.tsx
@@ -5,7 +5,7 @@ import { localization } from '../../localization';
 export function GameOverlay({ score }: { score: number }): React.ReactNode {
   const [bestScore] = useHighScore();
   return (
-    <div className="margin-2 padding-2 absolute bottom-0 rounded bg-white/40 text-4xl">
+    <div className="margin-2 padding-2 absolute bottom-0 rounded-sm bg-white/40 text-4xl">
       {localization.score}
       <span className={score > bestScore ? 'text-red-500' : undefined}>
         {score}
